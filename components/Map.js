75e7376a944bb53ac8ec835acb24f0ccd227e9a9
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-11 01:52:49 -0500

    minor text edit

--- components/Map.js
+++ components/Map.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Map = () => {
     return (
-        <div className="h-auto border-2 rounded-md border-gray-700 flex-1">
-            <h1>Map container</h1>
+        <div className="h-auto border-2 border-dotted border-gray-700 flex-1">
+            <h1>Mapbox goes here</h1>
         </div>
     )
 }
