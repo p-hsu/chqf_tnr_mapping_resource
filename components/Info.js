6df4b40f0a002e3f125b7f90f62e28e3c50b6299
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-11 01:53:14 -0500

    minor css edit

--- components/Info.js
+++ components/Info.js
@@ -4,7 +4,7 @@ import Colony from './Colony';
 
 const Info = () => {
     return (
-        <div className=" border-2 rounded-md border-gray-700 flex flex-1 grid grid-row-2 gap-1 overflow-auto overscroll-y-contain">
+        <div className="flex flex-1 grid grid-row-2 gap-1">
             <Client />
             <Colony />
 
