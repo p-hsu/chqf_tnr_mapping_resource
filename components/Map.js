2b58b1942c5a4bca41b847d150261b2e06767e6a
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-11 01:02:20 -0500

    added map container

---
+++ components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () => {
+    return (
+        <div className="h-auto border-2 rounded-md border-gray-700 flex-1">
+            <h1>Map container</h1>
+        </div>
+    )
+}
+
+export default Map
