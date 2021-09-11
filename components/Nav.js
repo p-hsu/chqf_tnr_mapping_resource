4e3edf09750e6c0cada3fde48d232c01d15223cd
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-10 23:14:16 -0500

    mobile first UIUX

--- components/Nav.js
+++ components/Nav.js
@@ -3,12 +3,15 @@ import Link from 'next/link';
 
 const Nav = () => {
     return (
-        <div className="py-4 px-8 bg-gray-700 text-yellow-400 flex justify-between">
-            <div className="text-xl"><Link href="/">CHQF TNR MAP</Link></div>
+        <div className="py-2 px-3 bg-gray-700 text-gray-300 flex justify-between">
+            <div className="text-xl p-1 border-2 rounded-md border-dotted border-gray-900 border-opacity-75 text-purple-300"><Link href="/">CHQF TNR MAP</Link></div>
             <div className="flex items-center">
-                <div className="ml-8"><Link href="/add">Add</Link></div>
-                <div className="ml-8"><Link href="/about">About</Link></div>
-                <div className="ml-8 p-1 rounded-md bg-gray-600 bg-opacity-60 text-purple-400"><a target="_blank" href="https://clinichq.com/account/login">C-HQ</a></div>
+                {/* internal routes */}
+                <div className=""><Link href="/add">Add</Link></div>
+                <div className="ml-3"><Link href="/about">About</Link></div>
+                {/* external link to clinic hq cloud software used by CHQF */}
+                <div className="ml-2 text-xs text-gray-900">|</div>
+                <div className="ml-2 p-1 rounded-md bg-gray-600 bg-opacity-60 text-pink-400"><a target="_blank" href="https://clinichq.com/account/login">C-HQ</a></div>
             </div>
         </div>
     )
