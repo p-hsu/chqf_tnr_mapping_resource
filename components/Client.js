c23bb23fe68f38d2bd1511e967ad5d528b7c4b41
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-11 01:53:30 -0500

    dummy text included

--- components/Client.js
+++ components/Client.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Client = () => {
     return (
-        <div className="h-auto m-1 border-2 rounded-md border-gray-700 flex-auto">
-            <h1>Client container</h1>
+        <div className="h-auto m-1 px-2 border-2 rounded-md border-gray-700 bg-blue-300 bg-opacity-25 flex-auto text-base">
+            <h1 className="text-lg">Client name</h1>
+            <p className="text-xs"> telephone # / email</p>
+            <p> address </p>
+            <p> notes</p>
         </div>
     )
 }
--- components/Colony.js
+++ components/Colony.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Colony = () => {
     return (
-        <div className="h-auto m-1 border-2 rounded-md border-gray-700 flex-auto">
-            <h1>Colony container</h1>
+        <div className="h-auto m-1 px-2 border-2 rounded-md border-gray-700 bg-blue-300 bg-opacity-25 flex-auto text-base">
+            <h1 className="text-lg">Colony name</h1>
+            <h3 >Status:<span className="bg-yellow-200"> Pending</span>/<span className="bg-green-300">Ongoing</span>/<span className="bg-pink-300">Completed</span></h3>
+            <div className="p-2 grid auto-cols-max grid-cols-2">
+                <p># of cats:</p>
+                <p className="grid auto-row-max grid-row-2"> Resources
+                    <p className="mt-1 text-xs">food:</p>
+                    <p className="mt-1 text-xs">houses:</p>
+                </p>
+            </div>
         </div>
     )
 }
