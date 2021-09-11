823fab82088f7274afc08136cca32271c7e9a2fa
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-11 01:53:04 -0500

    minor css edit

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import Footer from './Footer';
 // layout component that wraps around entire app regardless of route
 const Layout = ({ children }) => {
     return (
-        <div className="static h-screen flex flex-col">
+        <div className="static h-screen flex flex-col bg-pink-100 bg-opacity-25">
             <Nav />
             {children}
             <Footer />
