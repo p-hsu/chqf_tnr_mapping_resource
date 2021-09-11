ee0545ab8acb392aeda74e46a0a6415d5b6d3e7d
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-11 01:01:53 -0500

    added containers for info

---
+++ components/Client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Client = () => {
+    return (
+        <div className="h-auto m-1 border-2 rounded-md border-gray-700 flex-auto">
+            <h1>Client container</h1>
+        </div>
+    )
+}
+
+export default Client
---
+++ components/Colony.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Colony = () => {
+    return (
+        <div className="h-auto m-1 border-2 rounded-md border-gray-700 flex-auto">
+            <h1>Colony container</h1>
+        </div>
+    )
+}
+
+export default Colony
