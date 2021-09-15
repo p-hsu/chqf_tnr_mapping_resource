9c90031777c98ff4e817340de9d5b2c524b7e035
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-15 18:04:45 -0500

    form input fields added

---
+++ components/Form.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <form className="p-2 m-auto w-full border-2 border-gray-700 border-opacity-50 rounded-md bg-blue-200 bg-opacity-25 shadow-lg">
+                <div className="form-div grid grid-cols-2 gap-2">
+                    <input type="text" placeholder="First Name/Org. *" className="form-input"/>
+                    <input type="text" placeholder="Last Name" className="form-input"/>
+                </div>
+                <div className="form-div">
+                    <input type="text" placeholder="Street *" className="form-input"/>
+                </div>
+                <div className="form-div grid grid-cols-3 gap-2">
+                    <input type="text" placeholder="City *" className="form-input"/>
+                    <input type="text" placeholder="State *" className="form-input"/>
+                    <input type="text" placeholder="Zipcode *" className="form-input"/>
+                </div>
+                <div className="form-div">
+                    <input type="text" placeholder="Phone #" className="form-input"/>
+                </div>
+                <div className="form-div">
+                    <input type="text" placeholder="Email" className="form-input"/>
+                </div>
+                <div className="form-div">
+                    <label className="mt-1 ml-1 uppercase text-gray-700 text-xs font-semibold">Status</label>
+                </div>
+                <div className="form-div">
+                    <select className="form-input bg-white">
+                        <option>--</option>
+                        <option>Feeder</option>
+                        <option>Trapper</option>
+                        <option>Transporter</option>
+                        <option>Organization</option>
+                    </select>
+                </div>
+        </form>
+    )
+}
+
+export default Form
