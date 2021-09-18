366ae0ea4c70fadd25b1490427fd25934c7e2ada
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-18 14:16:42 -0500

    inital button component

---
+++ components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div>
+            <button type="submit" className="mt-3 mx-2 mb-1 py-1 px-3 rounded-md float-right bg-blue-200 border-2 border-blue-500 hover:bg-blue-300 focus:bg-blue-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-700">
+                ADD
+            </button>
+        </div>
+    )
+}
+
+export default Button
