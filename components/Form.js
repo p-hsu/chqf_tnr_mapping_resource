b83c668305b98f5e368f3cee317c173422a13879
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-18 12:11:07 -0500

    minor style edits

--- components/Form.js
+++ components/Form.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <form className="p-2 m-auto w-full border-2 border-gray-700 border-opacity-50 rounded-md bg-blue-200 bg-opacity-25 shadow-lg">
+        <form className="p-2 m-auto w-full border-2 border-gray-700 border-opacity-50 rounded-md bg-gradient-to-b from-gray-700 via-gray-500 to-gray-700 shadow-lg">
+                <div className="text-xs text-gray-200">
+                    <p>* required fields</p>
+                </div>
                 <div className="form-div grid grid-cols-2 gap-2">
                     <input type="text" placeholder="First Name/Org. *" className="form-input"/>
                     <input type="text" placeholder="Last Name" className="form-input"/>
@@ -22,7 +25,7 @@ const Form = () => {
                     <input type="text" placeholder="Email" className="form-input"/>
                 </div>
                 <div className="form-div">
-                    <label className="mt-1 ml-1 uppercase text-gray-700 text-xs font-semibold">Status</label>
+                    <label className="mt-1 ml-1 uppercase text-gray-200 text-xs font-semibold">Status</label>
                 </div>
                 <div className="form-div">
                     <select className="form-input bg-white">
