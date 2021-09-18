7c0deb05da55ae45db484dbe5cb8d213f27d1263
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-18 12:10:40 -0500

    minor style edits

--- components/Client.js
+++ components/Client.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Client = () => {
     return (
-        <div className="h-auto m-1 px-2 border-2 rounded-md border-gray-700 bg-blue-200 bg-opacity-25 flex-auto text-base">
+        <div className="h-auto m-1 px-2 border-2 rounded-md border-gray-700 text-gray-200 bg-gradient-to-b from-gray-700 to-gray-500 flex-auto text-base shadow-lg">
             <h1 className="text-lg">Client name</h1>
             <p className="text-xs"> telephone # / email</p>
             <p> address </p>
