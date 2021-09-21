1b03da2389f4e04daf87965d4c49b972b9edd84c
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-09-20 22:48:06 -0500

    sending data as props to Map component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import clientPromise from '../lib/mongodb';
+import geocodeAddress from '../lib/geocode';
 import Map from '../components/Map';
 import Info from '../components/Info';
 
@@ -22,8 +23,8 @@ export default function Home({ colony }) {
           <link href='https://api.mapbox.com/mapbox-gl-js/v2.4.1/mapbox-gl.css' rel='stylesheet' />
         </Head>
       <div className="box-border w-screen object-contain relative flex flex-grow grid grid-rows-2 gap-2">
-        <Map />
-        <Info colony={colony}/>
+        <Map colony={colony} />
+        <Info colony={colony} />
 
       </div>
     </>
@@ -33,16 +34,6 @@ export default function Home({ colony }) {
 export async function getServerSideProps(context) {
   const client = await clientPromise
   const db = client.db()
-
-  // client.db() will be the default database passed in the MONGODB_URI
-  // You can change the database by calling the client.db() function and specifying a database like:
-  // const db = client.db("myDatabase");
-  // Then you can execute queries against your database like so:
-  // db.find({}) or any of the MongoDB Node Driver commands
-
-  // const isConnected = await client.isConnected()
-
-  // const { db } = await clientPromise
 
   // setting variable data as collection
   // const clientData = await db.collection("client").find().toArray();
@@ -59,18 +50,18 @@ export async function getServerSideProps(context) {
   // })
 
   const colonyData = await db.collection("colony").find().toArray();
-
+  
   const colony = colonyData.map(colony => {
     return{
       name: colony.name,
       address: colony.address,
+      coordinates: colony.location.coordinates,
       status: colony.status.code,
       total: colony.status.total,
       fixed: colony.status.fixed,
       rescue: colony.status.to_rescue
     }
   })
-
 
   return {
     props: { colony },
