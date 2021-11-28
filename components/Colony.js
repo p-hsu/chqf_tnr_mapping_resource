3ee5c27eb36a465a4863cca6b89d575b53a3eae5
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-11-28 12:26:42 -0600

    both pins showing on map by manual db input

--- components/Colony.js
+++ components/Colony.js
@@ -4,7 +4,8 @@ const Colony = ({colony}) => {
     return (
         <div className="h-auto m-1 px-2 border-2 rounded-md border-gray-700 text-gray-200 bg-gradient-to-b from-gray-700 to-gray-500 flex-auto text-base shadow-lg">
             <h1 className="text-lg">{colony.name}</h1>
-            <h3 >Status:<span className="bg-yellow-400"> Pending</span>/<span className="bg-green-600">Ongoing</span>/<span className="bg-pink-400">Completed</span></h3>
+            {/* <h3 >Status:<span className="bg-yellow-400"> Pending</span>/<span className="bg-green-600">Ongoing</span>/<span className="bg-pink-400">Completed</span></h3> */}
+            <h3>Status: {colony.status}</h3>
             <div className="p-2 grid auto-cols-max grid-cols-2">
                 <p>{colony.fixed} / {colony.total} fixed</p>
                 <p>{colony.rescue} / {colony.total} to rescues</p>
