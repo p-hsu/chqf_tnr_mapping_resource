a278b67f1593fdb1c749ffbd650d16e9ce9348d2
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-12-12 16:15:33 -0600

    symbol layer for markers

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
+import grnMarker from '../assets/markers/grn.png';
 
 const Map = ({ colony }) => {
 
-    // const [dataLoaded, setDataLoaded] = useState(false);
-
+    // mapbox token
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY
 
     const mapContainer = useRef(null)
+    const [map, setMap] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // set new instance of map inside container
@@ -16,13 +18,21 @@ const Map = ({ colony }) => {
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [-90.2191521696627, 38.62024012842884],
             zoom: 11,
-        })
+        });
 
         // checking if data is coming through
         console.log("coordinates >>>  " + colony.coordinates)
 
-        // add sources and layers after map full loads
+        // add images and sources to symbol layer
         map.on('load', () => {
+
+            // load and add marker image from assets
+            // map.loadImage(
+            //     'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png'
+            //     , (err, img) => {
+            //     if (err) throw err;
+            //     map.addImage('marker', img);
+            // });
 
             // add colony coordinates as geoJSON data
             map.addSource('colony-coordinates', {
@@ -33,30 +43,73 @@ const Map = ({ colony }) => {
                         'type': 'Point',
                         'coordinates': [colony.coordinates[0], colony.coordinates[1]]
                     },
+                    'properties': {
+                        'title':[colony.name],
+                        'status':[colony.status]
+                    }
                 }
-            })
-        });
+            });
 
-        // add layer to display coordinates as markers
-        map.addLayer({
-            id: 'colony-markers',
-            type: 'circle',
-            source: 'colony-coordinates',
-            paint: {
-                'circle-color': '#250a5e',
-                'circle-radius': 6,
-                'circle-stroke-color':'#e8e4f0',
-                'circle-stroke-width': 1
-            }
-        });
+            // ** SHOULD BE SETTING DATA THEN RE-RENDERING MAP...? **
+            //     const geojsonSource = map.getSource('colony-coordinates');
+            //     // update data after geoJSON source created
+            //     geojsonSource.setData({
+            //         'type':'Feature',
+            //         'geometry': {
+            //             'type': 'Point',
+            //             'coordinates': [colony.coordinates[0], colony.coordinates[1]]
+            //         },
+            //         'properties': {
+            //             'title':[colony.name]
+            //         }
+            //     })
 
-        //set data loaded to true after sources and layers
-        // setDataLoaded(true)
+                    // add layer to display coordinates as markers
+            // map.addLayer({
+            //     id: 'colony-markers',
+            //     type: 'symbol',
+            //     source: 'colony-coordinates',
+            //     layout: {
+            //         'icon-image': 'marker'
+            //     }
+            // });
+
+            // add layer to display coordinates as circles instead of markers....?
+            map.addLayer({
+                'id': 'locations',
+                'type': 'circle',
+                'source': 'colony-coordinates',
+                'paint': {
+                    // make circles larger as user zooms from z12 to z22
+                    'circle-radius': {
+                        'base': 2,
+                        'stops':[
+                            [12, 2],
+                            [22, 180]
+                        ]
+                    },
+                    // color circles by status property using 'match' and 'get' expressions
+                    'circle-color': 'rgb(146, 54, 238)',
+                    // [
+                    //     'match',['get', 'status'],
+                    //     'Complete','rgb(146, 54, 238)',
+                    //     'Ongoing','rgb(87, 230, 44)',
+                    //     'Pending','rgb(238, 176, 54)',
+                    //     'rgb(0, 0, 0)'
+                    // ]
+                    'circle-stroke-width': 0.5,
+                    'circle-stroke-color': 'rgb(0, 0, 0)'
+                }
+            });
+
+            setMap(map);
+            setLoading(false);
+            console.log("Data loaded!")
+
+        });
 
         // clean map on unmount
         return () => map.remove()
-
-
     }, [])
 
     // const geoLocateStyle = {
@@ -73,7 +126,6 @@ const Map = ({ colony }) => {
 
     return (
         <div className='h-auto border-2 bo{rder-dotted border-gray-700 flex-1'>
-            {/* <div style={dataLoaded ? undefined : {display: 'none'}} /> */}
             <div
                 ref={mapContainer}
                 style={{width: '100%', height: '100vh'}}
