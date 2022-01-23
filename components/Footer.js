afdb16d286e6273615f235ef05f371ef24a1ff7e
Pam Hsu <pamela.jk.hsu@gmail.com>
2022-01-23 14:33:16 -0600

    fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="container py-1 px-8 my-0 mx-auto bg-gray-700 text-sm text-gray-400 text-center flex-shrink">
+        <div className="py-1 px-8 my-0 mx-0 w-full bg-gray-700 text-sm text-gray-400 text-center">
             <p>© 2021 Pamela Hsu</p>
         </div>
     )
