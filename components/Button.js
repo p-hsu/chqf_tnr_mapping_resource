a1b8dbd29a81e091866dfc03c239f94d35f1903b
Pam Hsu <pamela.jk.hsu@gmail.com>
2022-01-23 14:22:59 -0600

    button UI

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = () => {
     return (
         <div>
-            <button type="submit" className="mt-2 mx-2 mb-1 py-1 px-3 rounded-md float-right bg-blue-200 border-2 border-blue-500 hover:bg-blue-300 focus:bg-blue-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-700">
+            <button type="submit" className="mt-2 mx-2 mb-1 py-1 px-3 rounded-md float-right bg-blue-200 border-2 border-blue-800 hover:bg-blue-900 hover:text-gray-200 focus:bg-blue-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-700">
                 ADD
             </button>
         </div>
